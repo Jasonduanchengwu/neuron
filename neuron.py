068fc008c1d31beb69de56f1efcd08149ab65365
jasonduanchengwu <duanchengwu001@gmail.com>
2022-09-25 14:20:58 -0700

    adding training and testing set

--- neuron.py
+++ neuron.py
@@ -1,1 +1,20 @@
-print("ok")
+from random import random
+from io import open
+from pathlib import Path
+
+class neuronBot:
+    def __init__(self) -> None:
+        self.weights = [[0 for i in range(2)] for j in range(10)]
+        
+    def read_data(self, file_path):
+        training_file = open(file_path, "r")
+        training_file.readline()
+
+def main():
+    neuron_bot = neuronBot()
+    file_path = Path('nnTrainData.txt')
+    neuron_bot.read_data(str(file_path))
+    return
+
+if __name__ == '__main__':
+    main()
